74992c5aaa62c2468b46112f6fc35112e52e3f64
Manisha Singh <80120667+manisha-singh22@users.noreply.github.com>
2022-03-30 16:42:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,6 @@ App = {
     } else {
       window.alert("Please connect to Metamask.")
     }
-    // Modern dapp browsers...
     if (window.ethereum) {
       window.web3 = new Web3(ethereum)
       try {
@@ -30,14 +29,12 @@ App = {
         // User denied account access...
       }
     }
-    // Legacy dapp browsers...
     else if (window.web3) {
       App.web3Provider = web3.currentProvider
       window.web3 = new Web3(web3.currentProvider)
       // Acccounts always exposed
       web3.eth.sendTransaction({/* ... */})
     }
-    // Non-dapp browsers...
     else {
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
